11cb2e25afda9c966c745cc3702a451f66e17e8e
Raymond Siring <raymond.siring@weq.no>
2017-11-17 13:08:14 +0100

    removed config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.botToken = "MzQ4NjA2MjIxOTcwMDQ2OTg2.DHr24w.VRdJR1wNq4g9xcXqr_qxVWH_kNs";
-config.superAdmin = "305105753067225090"; // Snowflake weq
-config.debugMode = true;
-
-module.exports = config;
